82e31087f322c9f2456fcf25a9a8ffec7997c53a
Suraj Poudel <surajpoudelg@gmail.com>
2024-04-16 21:06:15 +1000

    Initial Commit

---
+++ MovieSwiftUIApp/MovieSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSwiftUIAppApp.swift
+//  MovieSwiftUIApp
+//
+//  Created by suraj poudel on 16/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct MovieSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
